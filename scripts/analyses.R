2e7799cf49f0b79acc4cadee8d52b634c3c4065b
NicolasRuth <drnicolasruth@gmail.com>
2024-08-25 21:49:50 +0200

    new figures and script for figure plotting

--- scripts/analyses.R
+++ scripts/analyses.R
@@ -23,6 +23,21 @@ plot_yearly + geom_bar(aes(fill = magazine)) +
        x = "Years",
        y = "Number of articles")
 
+# alternativ transparent bg
+
+plot_yearly <- ggplot(df, aes(as.factor(year))) +
+  geom_bar(aes(fill = magazine)) +
+  labs(fill = "Magazines",
+       x = "Jahre",
+       y = "Anzahl der Artikel") +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        text = element_text(size = 18),
+        axis.title = element_text(size = 18),
+        axis.text = element_text(size = 18))
+
+ggsave("plot_yearly.png", plot = plot_yearly, bg = "transparent", width = 8, height = 4)
 
 
 ## Differences between magazines
@@ -34,6 +49,7 @@ df_by_magazine <- df %>%
 
 df_by_magazine <- rbind(df_by_magazine, c("Musik & Bildung", 0))
 df_by_magazine <- rbind(df_by_magazine, c("Rondo", 0))
+df_by_magazine <- rbind(df_by_magazine, c("Sound & Recording", 0))
 df_by_magazine$number <- as.numeric(df_by_magazine$number)
 
 plot_by_magazine <- ggplot(df_by_magazine,
@@ -42,15 +58,15 @@ plot_by_magazine <- ggplot(df_by_magazine,
   coord_flip() +
   labs(x = "Magazines",
        y = "Number of articles") +
-  scale_fill_brewer(palette = "Reds") +
+  scale_fill_brewer(palette = "Blues") +
   theme(legend.position = "none")
 
 plot_by_magazine
 
-# 2. Web3 as mweb3n or sub theme
+# 2. Web3 as main or sub theme
 
 df_mweb3n_vs_sub <- df %>%
-  group_by(mweb3n_sub) %>%
+  group_by(main_sub) %>%
   summarise(Anzahl = n())
 
 df_mweb3n_vs_sub # 1 = mweb3n, 2 = subtheme
@@ -58,7 +74,7 @@ df_mweb3n_vs_sub # 1 = mweb3n, 2 = subtheme
 # 3. Specific Web3 topics
 
 df_web3_topics <- df %>%
-  select(web3_topic_1, web3_topic_2, web3_topic_3) %>%
+  select(w3_topic_1, w3_topic_2, w3_topic_3) %>%
   gather(key = "Category", value = "Topic") %>%
   group_by(Topic) %>%
   summarise(Number = n()) %>%
@@ -107,6 +123,9 @@ df_framing <- df %>%
   group_by(framing) %>%
   summarise(Number = n())
 
+mean(df$framing)
+sd(df$framing)
+
 df_framing
 
 plot_framing <- ggplot(df_framing,
@@ -114,10 +133,30 @@ plot_framing <- ggplot(df_framing,
                            y = Number,
                        fill = factor(framing))) +
   geom_bar(stat = "identity") +
-  scale_fill_brewer(palette = "Blues") +
+  scale_fill_brewer(palette = "Reds") +
   labs(x = "Negative to positive framing",
        y = "Number of articles") +
   theme(legend.position = "none")
+
+# alternativ trans bg
+
+plot_framing <- ggplot(df_framing,
+                       aes(x = framing,
+                           y = Number,
+                           fill = factor(framing))) +
+  geom_bar(stat = "identity") +
+  scale_fill_brewer(palette = "Oranges") +
+  labs(x = "Negatives bis positives Framing",
+       y = "Anzahl der Artikel") +
+  theme(legend.position = "none") +
+  theme(panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        text = element_text(size = 18),
+        axis.title = element_text(size = 18),
+        axis.text = element_text(size = 18))
+
+ggsave("plot_framing.png", plot = plot_framing, bg = "transparent", width = 6, height = 4)
+
 
 plot_framing
 
@@ -126,7 +165,8 @@ xtabs(~df$framing+df$magazine)
 group_by(df, magazine) %>%
   summarise(
     mean = mean(framing, na.rm = TRUE),
-    sd = sd(framing, na.rm = TRUE)
+    sd = sd(framing, na.rm = TRUE),
+    number = count(framing, na.rm = TRUE)
   ) %>%
   arrange(-mean)
 
@@ -140,7 +180,7 @@ df_easeofuse
 
 plot_easeofuse <- ggplot(df_easeofuse,
                          aes(x = eou_tam, y = Number)) +
-  geom_bar(stat = "identity", fill = "brown3") +
+  geom_bar(stat = "identity", fill = "darkolivegreen2") +
   ylim(0, 10) +
   labs(x = "Perceived ease of use (TAM)",
        y = "Number of articles")
@@ -156,7 +196,7 @@ df_usability
 
 plot_usability <- ggplot(df_usability, aes(x = usa_tam,
                                              y = Number)) +
-  geom_bar(stat = "identity", fill = "cornsilk3") +
+  geom_bar(stat = "identity", fill = "darkolivegreen3") +
   labs(x = "Perceived usability (TAM)",
        y = "Number of articles")
 
@@ -171,7 +211,7 @@ df_enjoyment
 
 plot_enjoyment <- ggplot(df_enjoyment, aes(x = enj_tam,
                                            y = Number)) +
-  geom_bar(stat = "identity", fill = "darksalmon") +
+  geom_bar(stat = "identity", fill = "darkolivegreen4") +
   labs(x = "Perceived enjoyment (TAM)",
        y = "Number of articles")
 
@@ -181,3 +221,47 @@ combinedTAM <- ggarrange(plot_easeofuse, plot_usability, plot_enjoyment,
                            ncol = 3, nrow = 1)
 
 combinedTAM
+
+# alternative in trans bg
+
+plot_easeofuse <- ggplot(df_easeofuse,
+                         aes(x = eou_tam, y = Number)) +
+  geom_bar(stat = "identity", fill = "darkolivegreen2") +
+  ylim(0, 10) +
+  labs(x = "Wg. Benutzerfreundlichkeit (TAM)",
+       y = "Anzahl der Artikel") +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        text = element_text(size = 18),
+        axis.title = element_text(size = 18),
+        axis.text = element_text(size = 18))
+
+plot_usability <- ggplot(df_usability, aes(x = usa_tam,
+                                           y = Number)) +
+  geom_bar(stat = "identity", fill = "darkolivegreen3") +
+  labs(x = "Wg. Nützlichkeit (TAM)",
+       y = "Anzahl der Artikel") +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        text = element_text(size = 18),
+        axis.title = element_text(size = 18),
+        axis.text = element_text(size = 18))
+
+plot_enjoyment <- ggplot(df_enjoyment, aes(x = enj_tam,
+                                           y = Number)) +
+  geom_bar(stat = "identity", fill = "darkolivegreen4") +
+  labs(x = "Wg. Unterhaltungswert (TAM)",
+       y = "Anzahl der Artikel") +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        text = element_text(size = 18),
+        axis.title = element_text(size = 18),
+        axis.text = element_text(size = 18))
+
+combinedTAM_trans <- ggarrange(plot_easeofuse, plot_usability, plot_enjoyment,
+                         ncol = 3, nrow = 1)
+
+ggsave("combinedTAM.png", plot = combinedTAM_trans, bg = "transparent", width = 14, height = 4)
